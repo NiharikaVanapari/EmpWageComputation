5418c89fffd5a237cb2422a20d2b4041da5ba909
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-04-28 21:51:35 +0530

    Added UC1

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,6 +4,13 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage computation program");
+		 int empPresent = 1;
+	        int empCheck = (int) (Math.random() * 10) % 2;
+	        if (empCheck == empPresent) {
+	            System.out.println("Employee Present");
+	        } else {
+	            System.out.println("Employee Absent");
+	        }
 		
 	}
 
